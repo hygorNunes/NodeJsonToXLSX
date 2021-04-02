4e4018a4398cb03c58247e0a06ea2ae0219527fd
Hygor Silva Nunes dos Santos <hsnsantos@uneb.br>
2021-04-01 22:27:58 -0300

    fix: caminho dinamico para salvar os arquivos

--- src/controllers/DocsController.js
+++ src/controllers/DocsController.js
@@ -1,4 +1,6 @@
 const reader = require('xlsx')
+const fs = require('fs')
+const path = require('path')
 const moment  = require('moment')
 
 class DocsController {
@@ -21,22 +23,34 @@ class DocsController {
 
     transformToXLS(req, res) {
         try {
-            let diaDeHoje = moment(new Date()).format("DD/MM/YYYY")
+            let diaDeHoje = moment(new Date()).format("DD_MM_yyyy")
             let dados = req.body.conteudo
-    
-            const ws = reader.utils.json_to_sheet(dados);
-            const wb = reader.utils.book_new()
-        
-            reader.utils.book_append_sheet(wb, ws, "Dados")
-    
+            
             let originalFileName = req.body.titulo
-            let exportFileName = originalFileName + '_' + diaDeHoje
-            let path =  'D:/Sistemas/playground/NodeJsonToXLSX/assets/sheets' + exportFileName;
-    
-            // Writing to our file 
-            reader.writeFile(wb, "D:/Sistemas/playground/NodeJsonToXLSX/assets/sheets/exportFileName.xlsx")
+            let exportFileName = originalFileName + '_' + diaDeHoje + '.xls'
+            
+            
+            let dirPath = path.join(__dirname, '../../assets/sheets');//criando o caminho do diretorio
+            var fd = fs.openSync(path.join(dirPath, exportFileName), 'w', 0o666); //criando um novo arquivo
 
-            res.status(200).send({message: 'Tudo certo na criação do seu arquivo xlsx'})
+            let filePath = path.resolve(dirPath, exportFileName); //pegando o caminho do arquivo criado
+            
+            const file = reader.readFile(filePath, {type: 'binary', cellDates: true, dateNF: 'yyyy/mm/dd;@'}); //lendo arquivo
+            const ws = reader.utils.json_to_sheet(dados); //tranformando json em xls
+            reader.utils.book_append_sheet(file, ws, "Dados") //adicionando uma planilha ao arquivo
+    
+            //Escrevendo nosso arquivo
+            reader.writeFile(file, filePath)
+
+            setTimeout(function () {
+                // Its printed after the file is closed
+                console.log('fechando arquivo');
+                
+                // closing file descriptor
+                fs.closeSync(fd);
+              }, 1000);
+
+            res.status(200).send({message: `Tudo certo na criação do seu arquivo .xlsx: ${exportFileName}`})
 
         } catch(e) {
             res.status(500).send({message: 'Error Get: ' + e.message})
